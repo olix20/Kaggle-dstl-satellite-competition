1ccb2bd8917e5d450fd7a281ad7d47048e949c5d
Armin Oliya <md.oliya@gmail.com>
2017-05-05 08:14:38 +0200

    code dump

---
+++ check.py
@@ -0,0 +1,135 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import pandas as pd
+from shapely.wkt import loads as wkt_loads
+import tifffile as tiff
+import os
+import random
+from keras.models import Model
+from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D, Reshape, core, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as K
+from sklearn.metrics import jaccard_similarity_score
+from shapely.geometry import MultiPolygon, Polygon
+import shapely.wkt
+import shapely.affinity
+from collections import defaultdict
+
+inDir = "data"
+
+
+def mask_for_polygons(polygons, im_size):
+    # __author__ = Konstantin Lopuhin
+    # https://www.kaggle.com/lopuhin/dstl-satellite-imagery-feature-detection/full-pipeline-demo-poly-pixels-ml-poly
+    img_mask = np.zeros(im_size, np.uint8)
+    if not polygons:
+        return img_mask
+    int_coords = lambda x: np.array(x).round().astype(np.int32)
+    exteriors = [int_coords(poly.exterior.coords) for poly in polygons]
+    interiors = [int_coords(pi.coords) for poly in polygons
+                 for pi in poly.interiors]
+    cv2.fillPoly(img_mask, exteriors, 1)
+    cv2.fillPoly(img_mask, interiors, 0)
+    return img_mask
+
+
+
+def mask_to_polygons(mask, epsilon=5, min_area=1.):
+    # __author__ = Konstantin Lopuhin
+    # https://www.kaggle.com/lopuhin/dstl-satellite-imagery-feature-detection/full-pipeline-demo-poly-pixels-ml-poly
+
+    # first, find contours with cv2: it's much faster than shapely
+    image, contours, hierarchy = cv2.findContours(
+        ((mask == 1) * 255).astype(np.uint8),
+        cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
+    # create approximate contours to have reasonable submission size
+    approx_contours = [cv2.approxPolyDP(cnt, epsilon, True)
+                       for cnt in contours]
+    if not contours:
+        return MultiPolygon()
+    # now messy stuff to associate parent and child contours
+    cnt_children = defaultdict(list)
+    child_contours = set()
+    assert hierarchy.shape[0] == 1
+    # http://docs.opencv.org/3.1.0/d9/d8b/tutorial_py_contours_hierarchy.html
+    for idx, (_, _, _, parent_idx) in enumerate(hierarchy[0]):
+        if parent_idx != -1:
+            child_contours.add(idx)
+            cnt_children[parent_idx].append(approx_contours[idx])
+    # create actual polygons filtering by area (removes artifacts)
+    all_polygons = []
+    for idx, cnt in enumerate(approx_contours):
+        if idx not in child_contours and cv2.contourArea(cnt) >= min_area:
+            assert cnt.shape[1] == 1
+            poly = Polygon(
+                shell=cnt[:, 0, :],
+                holes=[c[:, 0, :] for c in cnt_children.get(idx, [])
+                       if cv2.contourArea(c) >= min_area])
+            all_polygons.append(poly)
+    # approximating polygons might have created invalid ones, fix them
+    all_polygons = MultiPolygon(all_polygons)
+    if not all_polygons.is_valid:
+        all_polygons = all_polygons.buffer(0)
+        # Sometimes buffer() converts a simple Multipolygon to just a Polygon,
+        # need to keep it a Multi throughout
+        if all_polygons.type == 'Polygon':
+            all_polygons = MultiPolygon([all_polygons])
+    return all_polygons
+
+
+
+
+def M(image_id):
+    # __author__ = amaia
+    #   
+    filename = os.path.join(inDir, 'sixteen_band', '{}_M.tif'.format(image_id))
+    img = tiff.imread(filename)
+    img = np.rollaxis(img, 0, 3)
+    return img
+
+def check_predict(id='6010_0_0'):
+    # model = get_unet()
+    # model.load_weights('weights/unet_10_jk0.7878')
+    msk = np.load('msk/10_%s.npy' % id)
+
+
+    # msk = predict_id(id, model, [0.4, 0.1, 0.4, 0.3, 0.3, 0.5, 0.3, 0.6, 0.1, 0.1])
+    img = M(id)
+
+    for i in range(10):
+        print i, ":", len(msk[i]), np.sum(msk[i])
+
+
+    plt.figure()
+    ax1 = plt.subplot(131)
+    ax1.set_title('image ID:6120_2_3')
+    ax1.imshow(img[:, :, 4])#, cmap=plt.get_cmap('gist_ncar'))
+    ax2 = plt.subplot(132)
+    ax2.set_title('predict water pixels')
+    ax2.imshow(msk[5], cmap=plt.get_cmap('gray'))
+    ax3 = plt.subplot(133)
+    ax3.set_title('predict water polygones')
+    ax3.imshow(mask_for_polygons(mask_to_polygons(msk[6], epsilon=1), img.shape[:2]), cmap=plt.get_cmap('gray'))
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    # theano.config.openmp = True
+
+    # setGlobalParams()
+    # print "buffer limit: ", bufferLimit
+    # print "buffer counter: ", bufferCounter
+
+    # stick_all_train()
+    # make_val()
+    # model = train_net()
+
+    # score, trs = calc_jacc(model)
+    # predict_test(model, trs)
+    # make_submit()
+
+    # bonus
+    check_predict("6100_0_2")
